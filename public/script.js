7bf243cb7faca5cd0b503a67f1516c9f57b9a24f
Aenoris <aenoriss@gmail.com>
2021-10-28 01:26:59 -0300

    adsdsad

--- public/script.js
+++ public/script.js
@@ -2,10 +2,7 @@ const socket = io("/")
 const videoGrid = document.getElementById("video-grid");
 
 
-const myPeer = new Peer(undefined, {
-    host: "/",
-    port: "3001"
-});
+const myPeer = new Peer(undefined);
 
 const myVideo = document.createElement("video");
 myVideo.muted = true;
