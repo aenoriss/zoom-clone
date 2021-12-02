824910bd431b78dc2193ac845bd457d30ff47b64
Aenoris <aenoriss@gmail.com>
2021-12-02 19:38:18 -0300

    asdasd

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const THREE = require("three");
+
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 let userArr = {};
+let portalArr = [];
 let cometArr = [];
 let currentCometPos = [];
+let portalNum = 0;
 let room;
-const PORT = process.env.PORT || 4100;
+// const PORT = process.env.PORT || 4100;
+const PORT = 4100;
 
 //Generate coments
 
@@ -20,6 +27,7 @@ const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
 app.use(cors({ origin: "*" }));
+app.use(express.json())
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -28,18 +36,35 @@ app.get('/userArr', (req, res) => {
     res.send(userArr);
 });
 
+app.get('/portalArr', (req, res) => {
+    res.send(portalArr);
+});
+
 app.get('/lol', (req, res) => {
     res.send("done");
+});
+
+app.post('/portalCreation', (req, res) => {
+    let data = req.body;
+    if (portalArr.length > 0) {
+        portalNum = portalNum + 1
+        data.portal.id = "portal" + portalNum;
+    } else {
+        data.portal.id = "portal" + portalNum;
+    }
+
+    portalArr.push(data.portal);
+    console.log("see this", { portal: data.portal, array: portalArr, type: "create" })
+    userArr[data.user].score = userArr[data.user].score - 3
+    io.sockets.in(room).emit('portal-spawn', { portal: data.portal, array: portalArr, type: "create" });
+    res.send()
+
 });
 
 for (let i = 0; i < 50; i++) {
     currentCometPos.push(
         {
-            position: {
-                x: (Math.random() - 0.5) * 100,
-                y: (Math.random() - 0.5) * 100,
-                z: (Math.random() - 0.5) * 100,
-            },
+            position: new THREE.Vector3((Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100),
             rotation: {
                 x: Math.random() * Math.PI,
                 y: Math.random() * Math.PI
@@ -49,11 +74,7 @@ for (let i = 0; i < 50; i++) {
 
     cometArr.push(
         {
-            position: {
-                x: (Math.random() - 0.5) * 100,
-                y: (Math.random() - 0.5) * 100,
-                z: (Math.random() - 0.5) * 100,
-            },
+            position: new THREE.Vector3((Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100),
             rotation: {
                 x: Math.random() * Math.PI,
                 y: Math.random() * Math.PI
@@ -62,19 +83,12 @@ for (let i = 0; i < 50; i++) {
     )
 }
 
-io.sockets.in(room).emit('comet-position', cometArr);
-console.log("SEND")
-
 setInterval(function () {
     cometArr = [];
     for (let i = 0; i < 50; i++) {
         cometArr.push(
             {
-                position: {
-                    x: (Math.random() - 0.5) * 100,
-                    y: (Math.random() - 0.5) * 100,
-                    z: (Math.random() - 0.5) * 100,
-                },
+                position: new THREE.Vector3((Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100),
                 rotation: {
                     x: Math.random() * Math.PI,
                     y: Math.random() * Math.PI
@@ -86,26 +100,37 @@ setInterval(function () {
 
 
 setInterval(function () {
-    cometArr = [];
-    for (let i = 0; i < 50; i++) {
-        cometArr.push(
-            {
-                position: {
-                    x: (Math.random() - 0.5) * 100,
-                    y: (Math.random() - 0.5) * 100,
-                    z: (Math.random() - 0.5) * 100,
-                },
-                rotation: {
-                    x: Math.random() * Math.PI,
-                    y: Math.random() * Math.PI
-                }
+    currentCometPos.forEach((comet, x) => {
+        comet.position.lerp(cometArr[x].position, 0.0005)
+        for (const prop in userArr) {
+            if (userArr[prop].position.distanceTo(currentCometPos[x].position) < 1) {
+                currentCometPos[x].position = new THREE.Vector3((Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100, (Math.random() - 0.5) * 100)
+                userArr[prop].score = userArr[prop].score + 1;
+                console.log(userArr[prop].score)
+                io.sockets.in(room).emit('eating-sound', { id: prop, score: userArr[prop].score })
             }
-        )
-    }
-    io.sockets.in(room).emit('comet-position', cometArr);
-    console.log("SEND")
-}, 25000);
+        }
+    })
 
+
+
+    io.sockets.in(room).emit('comet-position', currentCometPos);
+}, 17);
+
+//Decaying
+setInterval(function () {
+    portalArr.forEach((portal, i) => {
+        portal.energy -= 1;
+        io.sockets.in(room).emit('portal-update', portalArr);
+        if (portal.energy <= 0) {
+            portalArr.splice(i, 1)
+            io.sockets.in(room).emit('portal-spawn', { array: portalArr, portal, type: "delete" });
+        }
+    })
+}, 1000);
+
+// io.sockets.in(room).emit('comet-position', cometArr);
+// console.log("SEND")
 
 io.on("connection", socket => {
     console.log("HEHE")
@@ -113,14 +138,18 @@ io.on("connection", socket => {
         room = roomId
         socket.join(roomId);
 
-        io.sockets.in(roomId).emit('comet-position', currentCometPos);
-        io.sockets.in(roomId).emit('comet-position', cometArr);
+        // io.sockets.in(roomId).emit('comet-position', currentCometPos);
+        // io.sockets.in(roomId).emit('comet-position', cometArr);
 
         //User Entity
-        userArr[userId] = { x: 0, y: 0, z: 0 }
+        userArr[userId] = {
+            position: new THREE.Vector3(0, 0, 0),
+            score: 0,
+        }
 
         //Another user joined the room
         socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.broadcast.to(roomId).emit('portal-pop', portalArr)
 
         socket.on("disconnect", () => {
             socket.broadcast.to(roomId).emit('user-disconnected', userId)
@@ -129,21 +158,29 @@ io.on("connection", socket => {
 
         socket.on("movement", (movement, user) => {
             // console.log("movement", movement.x, movement.y, movement.z, movement.user2);
-            userArr[user] = movement;
+            userArr[user].position = new THREE.Vector3(movement.x, movement.y, movement.z);
             io.sockets.in(roomId).emit('user-position', userArr);
         })
 
-        socket.on("comet", (cometMov) => {
-            currentCometPos = cometMov;
+        socket.on("portalCreation", (data) => {
+            // if (portalArr.length > 0) {
+            //     portalNum = portalNum + 1
+            //     data.portal.id = "portal" + portalNum;
+            // } else {
+            //     data.portal.id = "portal" + portalNum;
+            // }
+
+            // portalArr.push(data.portal);
+            // userArr[data.user].score = userArr[data.user].score - 3
+            // io.sockets.in(roomId).emit('portal-spawn', { portal: data.portal, array: portalArr, type: "create" });
         })
 
-        socket.on("cometPost", (cometMov) => {
-            currentCometPos = cometMov;
+        socket.on("portalFeeding", (data) => {
+            console.log(userArr[data.user]);
+            userArr[data.user].score = userArr[data.user].score - 1;
+            portalArr[data.portal].energy = portalArr[data.portal].energy + 15;
         })
-        
     })
 })
-
-
-
+ 
 server.listen(PORT);
